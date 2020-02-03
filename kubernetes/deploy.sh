401c1ddc4c71d8540da337d88adb962438a33d98
Moshe Demri <mdemri@gmail.com>
2020-02-03 17:58:54 +0200

    sleep better

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -5,6 +5,6 @@ NAMESPACE=monitoring
 ./01-configmap.sh $NAMESPACE
 kubectl -n $NAMESPACE apply -f ./02-rbac.yml
 kubectl -n $NAMESPACE apply -f ./03-graphite-node.yaml
-until kubectl get pods -n monitoring | grep 'graphite-node' | grep Running ; do date; sleep 1; echo ""; done
+until kubectl get pods -n monitoring | grep 'graphite-node' | grep Running ; do date; sleep 10; echo ""; done
 kubectl -n $NAMESPACE apply -f ./04-graphite-master.yaml
 kubectl -n $NAMESPACE apply -f ./05-carbon-relay.yaml
